bbf6a7550a8f386b503cfb749ead25afa38a1182
andrege <georgios.andreadakis@kvb.de>
2016-11-10 18:05:27 +0100

    First (unsuccessful) trials for test of the pact component.

---
+++ dev/mss/frontend/app/pact/pact.component.spec.js
@@ -0,0 +1,44 @@
+"use strict";
+var testing_1 = require('@angular/core/testing');
+var platform_browser_1 = require('@angular/platform-browser');
+var Observable_1 = require('rxjs/Observable');
+var mock_pact_1 = require('./mock-pact');
+var pact_component_1 = require('./pact.component');
+var pact_service_1 = require("./pact.service");
+var fixture;
+var comp;
+var de;
+var el;
+var pactService;
+var spy;
+describe('PactConmponent', function () {
+    beforeEach(testing_1.async(function () {
+        testing_1.TestBed.configureTestingModule({
+            declarations: [pact_component_1.PactComponent],
+            providers: [pact_service_1.PactService]
+        })
+            .compileComponents(); // compile template and css
+    }));
+    beforeEach(function () {
+        // create component and test fixture
+        fixture = testing_1.TestBed.createComponent(pact_component_1.PactComponent);
+        pactService = fixture.debugElement.injector.get(pact_service_1.PactService);
+        // Setup spy on the `getQuote` method
+        spy = spyOn(pactService, 'getPactObservable')
+            .and.returnValue(Observable_1.Observable.create(function (observer) { mock_pact_1.PACT; }));
+        // get test component from the fixture
+        comp = fixture.componentInstance;
+    });
+    it('should display a title', function () {
+        // trigger change detection to update the view
+        fixture.detectChanges();
+        fixture.whenStable().then(function () {
+            fixture.detectChanges(); // update view with quote
+            // query for the title <h1> by CSS element selector
+            de = fixture.debugElement.query(platform_browser_1.By.css('legend'));
+            // confirm the element's content
+            expect(de.nativeElement.textContent).not.toBe(null);
+        });
+    });
+});
+//# sourceMappingURL=pact.component.spec.js.map
---
+++ dev/mss/frontend/app/pact/pact.component.spec.ts
@@ -0,0 +1,58 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+
+import {Pact} from './pact';
+import {PACT} from './mock-pact'
+
+import { PactComponent } from './pact.component';
+import {PactService} from "./pact.service";
+import Spy = jasmine.Spy;
+
+let fixture: ComponentFixture<PactComponent>;
+let comp: PactComponent;
+
+let de: DebugElement;
+let el: HTMLElement;
+
+let pactService: PactService;
+let spy: Spy;
+
+describe('PactConmponent', () => {
+
+  beforeEach( async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PactComponent ],
+      providers: [ PactService ]
+    })
+      .compileComponents(); // compile template and css
+  }));
+
+  beforeEach(() => {
+
+    // create component and test fixture
+    fixture = TestBed.createComponent(PactComponent);
+
+    pactService = fixture.debugElement.injector.get(PactService);
+
+    // Setup spy on the `getQuote` method
+    spy = spyOn(pactService, 'getPactObservable')
+      .and.returnValue(Observable.create(function (observer) {PACT}));
+
+    // get test component from the fixture
+    comp = fixture.componentInstance;
+  });
+
+  it('should display a title', () => {
+    // trigger change detection to update the view
+    fixture.detectChanges();
+    fixture.whenStable().then(() => { // wait for async getQuote
+      fixture.detectChanges();        // update view with quote
+      // query for the title <h1> by CSS element selector
+      de = fixture.debugElement.query(By.css('legend'));
+      // confirm the element's content
+      expect(de.nativeElement.textContent).not.toBe(null);
+    });
+  });
+});
