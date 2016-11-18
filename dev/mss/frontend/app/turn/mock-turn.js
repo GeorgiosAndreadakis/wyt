fbb41527151ec37cb81148d8d262d6602e92b88f
andrege <georgios.andreadakis@kvb.de>
2016-11-18 09:52:51 +0100

    Removed mock instance for a turn.

--- dev/mss/frontend/app/turn/mock-turn.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.TURN = {
-    id: 1,
-    name: 'Carsten',
-    lastConfirmation: 'On May 2nd, 2016; Carsten turn; confirmed by himself'
-};
-//# sourceMappingURL=mock-turn.js.map
--- dev/mss/frontend/app/turn/mock-turn.ts
+++
@@ -1,7 +0,0 @@
-import { Turn } from './turn';
-
-export const TURN: Turn = {
-    id: 1,
-    name: 'Carsten',
-    lastConfirmation: 'On May 2nd, 2016; Carsten turn; confirmed by himself'
-};
